ccd2f88fb6bb09df6d6fe16b033b597e53e7fb0f
saharshaker <sahar.shaker200@gmail.com>
2025-10-15 17:22:21 +0330

    cleanup

--- terraform/oidc-connect-outputs.tf
+++ terraform/oidc-connect-outputs.tf
@@ -3,12 +3,10 @@ output "aws_iam_openid_connect_provider_arn" {
   value       = aws_iam_openid_connect_provider.oidc.arn
 }
 
-# # Extract OIDC Provider from OIDC Provider ARN
 locals {
   aws_iam_oidc_connect_provider_extract_from_arn = element(split("oidc-provider/", "${aws_iam_openid_connect_provider.oidc.arn}"), 1)
 }
 
-# Output: AWS IAM Open ID Connect Provider
 output "eks_oidc_provider_url_path" {
   description = "AWS IAM Open ID Connect Provider extract from ARN"
   value       = local.aws_iam_oidc_connect_provider_extract_from_arn
